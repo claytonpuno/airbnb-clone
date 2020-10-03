6435c0feed05d221cfebf9e45b7227489fead703
Clayton Puno <claytonpuno@gmail.com>
2020-10-02 21:32:15 -0400

    Updates

---
+++ precache-manifest.f36ca64b894ee9293ad8f4b9d5498530.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50afb8ce805aee0e9a4d96a6a5ff818",
+    "url": "/airbnb-clone/index.html"
+  },
+  {
+    "revision": "12eb445f9767021b7b0f",
+    "url": "/airbnb-clone/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "b45b0ec4bd049fa18b61",
+    "url": "/airbnb-clone/static/css/main.169d568f.chunk.css"
+  },
+  {
+    "revision": "12eb445f9767021b7b0f",
+    "url": "/airbnb-clone/static/js/2.47d4d71e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/airbnb-clone/static/js/2.47d4d71e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b45b0ec4bd049fa18b61",
+    "url": "/airbnb-clone/static/js/main.ce7e0fb5.chunk.js"
+  },
+  {
+    "revision": "f358a9c4d5787a3a04f2",
+    "url": "/airbnb-clone/static/js/runtime-main.fc586be8.js"
+  }
+]);
